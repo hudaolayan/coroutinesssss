83f19ac463375a57069a53e29e34e334943004f0
huda sha'foot <hudashafoot95@gmail.com>
2023-11-29 09:43:53 +0300

    Initial commit

---
+++ app/src/main/java/h/shafoot/coroutinesssss/MainActivity.kt
@@ -0,0 +1,57 @@
+package h.shafoot.coroutinesssss
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var hh:MainActivity
+    @OptIn(ExperimentalStdlibApi::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        lifecycleScope.launch(Dispatchers.Main) {
+            println("huda inspects threads ,,1  ${coroutineContext[CoroutineDispatcher]}")
+        }
+        lifecycleScope.launch(Dispatchers.Default) {
+            println("huda inspects threads ,,2  ${coroutineContext[CoroutineDispatcher]}")
+        }
+        lifecycleScope.launch(Dispatchers.IO) {
+            println("huda inspects threads ,,3  ${coroutineContext[CoroutineDispatcher]}")
+        }
+        lifecycleScope.launch {
+            println("huda inspects threads ,,4  ${coroutineContext[CoroutineDispatcher]}")
+        }
+
+        if (::hh.isInitialized){}
+
+
+
+
+
+
+
+    }
+
+
+    fun addNumbers(a: Int, b: Int): Int {
+        return a + b
+    }
+
+    fun main() {
+        val result = addNumbers(3, 4)
+        println("Result: $result")
+    }
+
+    fun mmm():(Int,Int)->Unit{
+        return {x,e->}
+    }
+
+    
+}
